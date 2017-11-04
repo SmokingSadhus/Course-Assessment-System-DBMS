1515626a64bcd17dd18547ffa34a9166c05726ed
SmokingSadhus <amedhek@ncsu.edu>
2017-11-04 07:45:58 -0400

    Professor View Courses

--- src/com/dbms/basiccheck/Professor.java
+++ src/com/dbms/basiccheck/Professor.java
@@ -1,11 +1,15 @@
 package com.dbms.basiccheck;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import oracle.jdbc.OracleTypes;
 
 public class Professor {
 	
@@ -39,6 +43,7 @@ public class Professor {
 		}
 		
 		int optionNo = sc.nextInt();
+		sc.nextLine();
 		String optionSelected = options.get(optionNo-1);
 		if(optionSelected.equals("Logout") || optionSelected.equals("Log out")){
 			Entry.startPage();
@@ -66,8 +71,100 @@ public class Professor {
 
 	private static void viewAddCourses() {
 		// TODO Auto-generated method stub
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		List<String> courses = new ArrayList<String>();
+		try{
+			stmt=con.prepareStatement("SELECT COURSE_ID from COURSE where PROFESSOR_ID = ?");
+			 stmt.setString(1, username);
+		     
+
+		 rs = stmt.executeQuery();
+		 while(rs.next()){
+			 courses.add(rs.getString("COURSE_ID")); 
+		 }
+		 
+		 }
+		catch(Exception e){
+		   e.printStackTrace();	
+		}
+		System.out.println("Couses you added: ");
+		for(int i=1; i <= courses.size();i++){
+		System.out.println(i + ": " + courses.get(i-1));	
+		}
+		
+		boolean done = false;
+		while(!done){
+		System.out.println("Enter Course ID to view the course");
+		System.out.println("Press 1 to add a new course");
+		System.out.println("Press 0 to go back to previous menu");
+		String ip = sc.nextLine();
+		//ip ="CSC 540";
+		//String ip = sc.next();
+		if(isNumber(ip)){
+			int no = Integer.parseInt(ip);
+			if(no == 1){
+				done = true;
+				System.out.println("add course");
+				//addCourse();
+			}
+			else if(no == 0){
+				done = true;
+				homePage();
+			}
+			
+		}else{
+			done = true;
+			//System.out.println("view specific course");
+			viewSpecificCourse(ip);
+		}
+		
+	}
+	}
+	
+	private static void viewSpecificCourse(String courseId) {
+		// TODO Auto-generated method stub
+		
+		String getRelevantOptions = "{call SELECT_PROFESSOR_OPTIONS(?,?,?)}";
+		CallableStatement callableStatement = null;
+		ResultSet rs = null;
+		try {
+			callableStatement = con.prepareCall(getRelevantOptions);
+			callableStatement.setString(1, courseId);
+			callableStatement.setString(2, username);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
+			callableStatement.executeUpdate();
+			rs = (ResultSet) callableStatement.getObject(3);
+			List<String> options = new ArrayList<String>();
+			while(rs.next()){
+				
+				System.out.println(rs.getString("col_name"));
+				//options.add(rs.getString("col_name"));
+			}
+			//String d = "k";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
+	}
+
+	private static void addCourse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static boolean isNumber(String x){
+		try
+	    {
+	        Integer.parseInt(x);
+	    }
+	    catch(NumberFormatException ex)
+	    {
+	        return false;
+	    }
+	    return true;
 	}
 
 	private static void viewProfile() {
