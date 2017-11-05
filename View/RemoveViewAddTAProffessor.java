eeef0bb00044ae16fe9aad937ea081d5e7a53cfc
Samir Jha <sjha4@ncsu.edu>
2017-11-05 12:32:38 -0500

    Update RemoveViewAddTAProffessor.java

--- View/RemoveViewAddTAProffessor.java
+++ View/RemoveViewAddTAProffessor.java
@@ -1,3 +1,8 @@
+	
+/*
+* View TA in specific Course
+* Choosing an existing TA deletes it from DB
+*/
 	private static void viewTA(String courseId) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -49,4 +54,56 @@
 			viewTA(courseId);
 		}
 		
+	}
+/*
+*Add TA to a coursee
+*/
+private static void addTA(String courseId) {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+			 stmt=con.prepareStatement("SELECT STUDENT_ID,NAME from STUDENT where IS_GRAD = ?");
+			 stmt.setInt(1, 1);
+		     rs = stmt.executeQuery();
+		     System.out.println("Choose 0 to return to course: "+courseId);
+		     List<String> students = new ArrayList<String>();
+		     while(rs.next()){
+		    	 students.add(rs.getString("STUDENT_ID"));
+			}
+		     for(int i=1; i <= tas.size();i++){
+				 System.out.println(i + ": " + students.get(i-1));
+			 }
+		     System.out.println("Choose student to add as TA or press 0 to return to course:" + courseId);
+			 int choice  = sc.nextInt();
+			 sc.nextLine();
+			 if(choice == 0){
+				 viewSpecificCourse(courseId);
+			 }
+			 else{
+				 String sId = students.get(choice-1);
+				 addTAtoCourse(sId,courseId);
+				 System.out.println("Added +"+sId+" as TA to course: "+courseId);
+				 viewSpecificCourse(courseId);
+			 }
+			
+		     
+		}
+		catch(Exception e){
+			System.out.println("Encountered Error: "+e.getMessage());
+			viewSpecificCourse(courseId);
+		}
+	}
+
+	private static void addTAtoCourse(String sId, String courseId) {
+		PreparedStatement stmt = null;
+		try{
+			stmt = con.prepareStatement("INSERT into TA (COURSE_ID,STUDENT_ID) values (?,?)");
+			stmt.setString(1, courseId);
+			stmt.setString(2, sId);
+			stmt.executeUpdate();
+		}
+		catch(Exception e){
+			System.out.println("Encountered Error: "+e.getMessage());
+			viewSpecificCourse(courseId);
+		}
 	}
