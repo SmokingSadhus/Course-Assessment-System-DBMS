69e1ff4256f324c63b58f3f948b1de83247cefd7
SmokingSadhus <amedhek@ncsu.edu>
2017-11-05 11:20:48 -0500

    new changes

--- src/com/dbms/basiccheck/Professor.java
+++ src/com/dbms/basiccheck/Professor.java
@@ -5,7 +5,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -281,7 +286,9 @@ public class Professor {
 		String [] sArray = params.split(",");
 		int ct = 0;
 		while(text.indexOf("<?>") != -1){
-            text = text.replace("<?>",sArray[ct]);
+            //text = text.replaceFirst("<?>",sArray[ct]);
+			int fIndex = text.indexOf("<?>");
+			text = text.substring(0,fIndex) + sArray[ct] + text.substring(fIndex+3); 
             ct++;
 		}
 		return text;
@@ -441,6 +448,346 @@ public class Professor {
 
 	private static void addExercise(String courseId) {
 		// TODO Auto-generated method stub
+		try{
+		System.out.println("1. Modify existing exercises");
+		System.out.println("2. Create new Exercise");
+		System.out.println("0. Go back to previous page");
+		int opt = sc.nextInt();
+		sc.nextLine();
+		if(opt == 0){
+			viewSpecificCourse(courseId);
+		}
+		else if(opt == 1){
+			modifyExistingExercises(courseId);
+		}
+		else if(opt == 2){
+		System.out.println("Enter Mode of the exercise: ");
+		String mode = sc.nextLine();
+		System.out.println("Enter name for this exercise: ");
+		String name = sc.nextLine();
+		System.out.println("Enter Deadline :");
+		String deadline = sc.nextLine();
+		System.out.println("Enter total number of questions");
+		int noQuestions = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter Retries");
+		int ret = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter Start Date: ");
+		String stDate = sc.nextLine();
+		System.out.println("Enter End Date: ");
+		String endDate = sc.nextLine();
+		System.out.println("Enter Points per question");
+		int points = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter Penalty");
+		int penalty = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter Scoring Policy");
+		String sp = sc.nextLine();
+		PreparedStatement stmt = null;
+		CallableStatement callableStatement = null;
+		ResultSet rs = null;
+		String retrieveQuestionId = "{call INSERT_EXERCISE_AND_RETURN_ID(?,?,?,?,?,?,?,?,?,?,?,?)}";
+		callableStatement = con.prepareCall(retrieveQuestionId);
+		callableStatement.setString(1, courseId);
+		callableStatement.setString(2, name);
+		callableStatement.setString(3, deadline);
+		callableStatement.setInt(4, noQuestions);
+		callableStatement.setInt(5, ret);
+		callableStatement.setString(6, stDate);
+		callableStatement.setString(7, endDate);
+		callableStatement.setInt(8, points);
+		callableStatement.setInt(9, penalty);
+		callableStatement.setString(10, sp);
+		callableStatement.setString(11, mode);
+		callableStatement.registerOutParameter(12, OracleTypes.CURSOR);
+		callableStatement.executeUpdate();
+		rs = (ResultSet) callableStatement.getObject(12);
+		int exId = 0; 
+		while(rs.next()){
+			exId = rs.getInt("ret_id");
+		}
+		cg.closeResult(rs);
+		cg.closeStatement(callableStatement);
+		//ResultSet rs = null;
+		 
+	     
+	     if(mode.equals("ADAPTIVE")){
+	    	 System.out.println("Mode is Adaptive so questions directly from Question Bank. Do you want to select a topic?");
+	    	 System.out.println("Press 1 for yes, 0 for no");
+	    	 int resp = sc.nextInt();
+	    	 sc.nextLine();
+	    	 if(resp == 1){
+	    		 System.out.println("select topic id's from bellow.");
+	    		 stmt=con.prepareStatement("select t.topic_id, t.name from course_topic ct inner join topic t on ct.topic_id = t.topic_id and ct.course_id = ?");
+				 stmt.setString(1, courseId);
+				 rs = stmt.executeQuery();
+				 while(rs.next()){
+						System.out.println(rs.getString("topic_id") + ": " + rs.getString("name"));
+					}
+				 cg.closeResult(rs);
+				 cg.closeStatement(stmt);
+				 String topicId = sc.nextLine();
+				 stmt=con.prepareStatement("insert into ADAPTIVE_EXERCISE_TOPIC values (?,?)");
+				 stmt.setInt(1, exId);
+				 stmt.setString(2, topicId);
+				 stmt.executeUpdate();
+				 System.out.println("Exercise crested successfully.");
+		    	 viewSpecificCourse(courseId);
+	    		 
+	    	 }
+	    	 else if (resp == 0){
+	    		System.out.println("Exercise crested successfully.");
+	    		viewSpecificCourse(courseId);
+	    	 }
+	    	 
+	     }
+	     else {
+	    	addQuestionsToExercise(courseId,noQuestions,exId); 
+	     }
+		}
+	    
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		//if(mode.)
+	}
+
+	
+	private static String dateConv(String start_dt){
+		
+		start_dt = start_dt.substring(0, 10);
+		String [] dArray = start_dt.split("-");
+		
+		String finalString= dArray[1] + "/"+dArray[2]+"/"+dArray[0];
+		
+		
+		return finalString;
+	}
+		private static void modifyExistingExercises(String courseId) {
+		// TODO Auto-generated method stub
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+		System.out.println("Please select exercise Id to modify");
+		stmt=con.prepareStatement("SELECT EXERCISE_ID, NAME, DEADLINE, TOTAL_QUESTIONS, RETRIES,START_DATE, END_DATE, POINTS, PENALTY, SCORING_POLICY, SC_MODE FROM EXERCISE where COURSE_ID = ?");
+		 stmt.setString(1, courseId);
+	     rs = stmt.executeQuery();
+	     class ExerciseInner{
+	    	 int exid;
+	    	 String name;
+	    	 String deadline;
+	    	 int noOfQuestions;
+	    	 int retries;
+	    	 String stDate;
+	    	 String endDate;
+	    	 int points;
+	    	 int penalty;
+	    	 String sp;
+	    	 String scMode;
+	    	public ExerciseInner(int exid, String name, String deadline,
+					int noOfQuestions, int retries, String stDate,
+					String endDate, int points, int penalty, String sp,
+					String scMode) {
+				super();
+				this.exid = exid;
+				this.name = name;
+				this.deadline = deadline;
+				this.noOfQuestions = noOfQuestions;
+				this.retries = retries;
+				this.stDate = stDate;
+				this.endDate = endDate;
+				this.points = points;
+				this.penalty = penalty;
+				this.sp = sp;
+				this.scMode = scMode;
+			}
+	    	 
+	     }
+	     HashMap<Integer,ExerciseInner> map = new HashMap<Integer,ExerciseInner>();
+	     while(rs.next()){
+	    	 System.out.println("Exercise ID: " + rs.getInt("EXERCISE_ID") + " Exercise Name: " + rs.getString("NAME") + " DEADLINE: " + rs.getString("DEADLINE") + " TOTAL_QUESTIONS: " + rs.getInt("TOTAL_QUESTIONS") + " RETRIES: " + rs.getInt("RETRIES") + " START DATE: " + rs.getString("START_DATE") + " END DATE: " + rs.getString("END_DATE") + " Points: " + rs.getInt("POINTS") + " PENALTY: " + rs.getInt("PENALTY") + " SCORING POLICY: " + rs.getString("SCORING_POLICY") + " MODE: " + rs.getString("SC_MODE"));
+	    	 ExerciseInner ei = new ExerciseInner(rs.getInt("EXERCISE_ID"),rs.getString("NAME"),dateConv(rs.getString("DEADLINE")),rs.getInt("TOTAL_QUESTIONS"),rs.getInt("RETRIES"),dateConv(rs.getString("START_DATE")),dateConv(rs.getString("END_DATE")),rs.getInt("POINTS"),rs.getInt("PENALTY"),rs.getString("SCORING_POLICY"),rs.getString("SC_MODE"));
+	    	 map.put(rs.getInt("EXERCISE_ID"), ei);
+		}
+	    int selectedId = sc.nextInt();
+	    sc.nextLine();
+	    ExerciseInner ei = map.get(selectedId);
+	    System.out.println("Either enter new value for field or press enter if you want to keep it unchanged");
+	    String newName;
+	    String newDeadline;
+	    int newTotal;
+	    int newRetries;
+	    String newStartDate;
+	    String newEndDate;
+	    int newPoints;
+	    int newPenalty;
+	    String newSP;
+	    String newMode;
+	    System.out.println("enter name");
+	    newName = sc.nextLine();
+	    if(newName.equals("")){
+	    	newName = ei.name;
+	    }
+	    System.out.println("enter deadline");
+	    newDeadline = sc.nextLine();
+	    if(newDeadline.equals("")){
+	    	newDeadline = ei.deadline;
+	    }
+	    System.out.println("Enter no of questions");
+	    String tempTotal = sc.nextLine();
+	    if(tempTotal.equals("")){
+	    	newTotal = ei.noOfQuestions;
+	    }
+	    else{
+	    	newTotal = Integer.parseInt(tempTotal);
+	    }
+	    System.out.println("Enter no of retries");
+	    String tempRetries = sc.nextLine();
+	    if(tempRetries.equals("")){
+	    	newRetries = ei.retries;
+	    }
+	    else{
+	    	newRetries = Integer.parseInt(tempRetries);
+	    }
+	    System.out.println("Enter StartDate");
+	    newStartDate = sc.nextLine();
+	    if(newStartDate.equals("")){
+	    	newStartDate = ei.stDate;
+	    }
+	    System.out.println("Enter EndDate");
+	    newEndDate = sc.nextLine();
+	    if(newEndDate.equals("")){
+	    	newEndDate = ei.endDate;
+	    }
+	    System.out.println("Enter number of points");
+	    String tempPoints = sc.nextLine();
+	    if(tempPoints.equals("")){
+	    	newPoints = ei.points;
+	    }
+	    else{
+	    	newPoints = Integer.parseInt(tempPoints);
+	    }
+	    System.out.println("Enter penalty");
+	    String tempPenalty = sc.nextLine();
+	    if(tempPenalty.equals("")){
+	    	newPenalty = ei.penalty;
+	    }
+	    else{
+	    	newPenalty = Integer.parseInt(tempPenalty);
+	    }
+	    System.out.println("Enter Scoring Policy");
+	    newSP = sc.nextLine();
+	    if(newSP.equals("")){
+	    	newSP = ei.sp;
+	    }   
+	    //System.out.println("Enter Mode");
+	    //newMode = sc.nextLine();
+	    newMode = ei.scMode;
+	    
+	    CallableStatement callableStatement = null;
+		rs = null;
+		String retrieveExerciseId = "{call INSERT_EXERCISE_AND_RETURN_ID(?,?,?,?,?,?,?,?,?,?,?,?)}";
+		callableStatement = con.prepareCall(retrieveExerciseId);
+		callableStatement.setString(1, courseId);
+		callableStatement.setString(2, newName);
+		callableStatement.setString(3, newDeadline);
+		callableStatement.setInt(4, newTotal);
+		callableStatement.setInt(5, newRetries);
+		callableStatement.setString(6, newStartDate);
+		callableStatement.setString(7, newEndDate);
+		callableStatement.setInt(8, newPoints);
+		callableStatement.setInt(9, newPenalty);
+		callableStatement.setString(10, newSP);
+		callableStatement.setString(11, newMode);
+		callableStatement.registerOutParameter(12, OracleTypes.CURSOR);
+		callableStatement.executeUpdate();
+		rs = (ResultSet) callableStatement.getObject(12);
+		
+		System.out.println("Successfully created new exercise.");
+	    viewSpecificCourse(courseId);
+	    
+		}
+	     catch(Exception e){
+	    	e.printStackTrace(); 
+	     }
+		
+	}
+
+	private static void addQuestionsToExercise(String courseId, int noQuestions, int exId) {
+		// TODO Auto-generated method stub
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+		for(int i =1; i <=  noQuestions; i++){
+			
+			System.out.println("How do you want to select question no: " + i);
+			System.out.println("1) Enter Question ID");
+			System.out.println("2) View Question Bank");
+			System.out.println("3) Enter Topic Id");
+			int choice = sc.nextInt();
+			sc.nextLine();
+			if(choice == 1){
+				System.out.println("Enter Question ID");
+				int qID = sc.nextInt();
+				stmt=con.prepareStatement("INSERT INTO EXERCISE_QUESTION values (?,?)");
+				 stmt.setInt(1, exId);
+				 stmt.setInt(2,  qID);
+				 stmt.executeUpdate();
+				 cg.closeStatement(stmt);
+				
+			}
+			else if(choice == 2 ){
+				System.out.println("Select question id from the question bank.");
+				stmt=con.prepareStatement("SELECT q.QUESTION_ID, q.QUESTION_TEXT from QUESTION_BANK qb inner join QUESTION q on qb.QUESTION_ID = q.QUESTION_ID where qb.COURSE_ID = ?");
+				stmt.setString(1, courseId);
+				rs = stmt.executeQuery();
+				while(rs.next()){
+					System.out.println(rs.getInt("QUESTION_ID") + ": " + rs.getString("QUESTION_TEXT"));
+				}
+				int qID = sc.nextInt();
+				sc.nextLine();
+				cg.closeResult(rs);
+				cg.closeStatement(stmt);
+				stmt=con.prepareStatement("INSERT INTO EXERCISE_QUESTION values (?,?)");
+				 stmt.setInt(1, exId);
+				 stmt.setInt(2,  qID);
+				 stmt.executeUpdate();
+				 cg.closeStatement(stmt);
+				
+			}
+			else{
+				System.out.println("Enter required topic id: ");
+				String tId = sc.nextLine();
+				System.out.println("Select required question id: ");
+				stmt=con.prepareStatement("select q.QUESTION_ID, q.QUESTION_TEXT from Question q where q.topic_id = ? ");
+				stmt.setString(1, tId);
+				rs = stmt.executeQuery();
+				while(rs.next()){
+					System.out.println(rs.getInt("QUESTION_ID") + ": " + rs.getString("QUESTION_TEXT"));
+				}
+				int qID = sc.nextInt();
+				sc.nextLine();
+				cg.closeResult(rs);
+				cg.closeStatement(stmt);
+				stmt=con.prepareStatement("INSERT INTO EXERCISE_QUESTION values (?,?)");
+				 stmt.setInt(1, exId);
+				 stmt.setInt(2,  qID);
+				 stmt.executeUpdate();
+				 cg.closeStatement(stmt);
+				
+			}
+					
+		}
+		
+		System.out.println("Created Exercise successfully.");
+		viewSpecificCourse(courseId);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 		
 		
 	}
@@ -489,13 +836,13 @@ public class Professor {
 		
 	}
 
-	private static void viewSpecificExercise(int no, String courseId) {
+	private static void viewSpecificExercise(int exId, String courseId) {
 		// TODO Auto-generated method stub
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try{
 		stmt=con.prepareStatement("SELECT SC_MODE FROM EXERCISE where EXERCISE_ID = ?");
-		 stmt.setInt(1, no);
+		 stmt.setInt(1, exId);
 	     rs = stmt.executeQuery();
 	     String mode = "";
 	     while(rs.next()){
@@ -505,6 +852,13 @@ public class Professor {
 	     cg.closeResult(rs);
 	     cg.closeStatement(stmt);
 	     if(mode.equals("ADAPTIVE")){
+	    	 stmt=con.prepareStatement("SELECT t.name FROM ADAPTIVE_EXERCISE_TOPIC aet inner join Topic t on aet.topic_id = t.topic_id  where aet.EXERCISE_ID = ?");
+	    	 stmt.setInt(1, exId);
+	    	 String topic = "";
+		     while(rs.next()){
+		    	 topic = rs.getString("name");
+		    	}
+		     System.out.println("Exercise is adaptive and topic is: " + topic);
 	    	 
 	     }
 	     else{
